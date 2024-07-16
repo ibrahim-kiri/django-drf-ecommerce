2721ec4ad7e045a777d1ffd0ed2c0a85e925e175
ibrahim-kiri <ibrahimkiri@gmail.com>
2024-07-16 16:22:08 +0300

    Developed and Tested the API

--- commands.md
+++ commands.md
@@ -1,3 +1,6 @@
 # generate random secret key
 from django.core.management.utils import get_random_secret_key
 print(get_random_secret_key())
+
+# Commands
+python manage.py spectacular --file schema.yml
--- drfecommerce/settings/base.py
+++ drfecommerce/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #External Packages
     'rest_framework',
+    'drf_spectacular',
     #Internal Apps
     'drfecommerce.product',
 ]
@@ -116,4 +117,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = {}
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Django DRF Ecommerce",
+    "DESCRIPTION": "API for managing products and orders in an e-commerce application.",
+}
